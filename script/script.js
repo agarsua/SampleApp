8d6964da41e0d2a5751e8a7c333c9b4e9b13fb04
agarsua <ianarsua@gmail.com>
2025-08-19 20:43:32 +0800

    publish test website

---
+++ script/script.js
@@ -0,0 +1,21 @@
+﻿const sidebar = document.getElementById("sidebar");
+const toggleBtn = document.getElementById("toggleBtn");
+
+let isCollapsed = false;
+
+toggleBtn.addEventListener("click", () => {
+    isCollapsed = !isCollapsed;
+    sidebar.classList.toggle("collapsed", isCollapsed);
+});
+
+sidebar.addEventListener("mouseenter", () => {
+    if (isCollapsed) {
+        sidebar.classList.remove("collapsed");
+    }
+});
+
+sidebar.addEventListener("mouseleave", () => {
+    if (isCollapsed) {
+        sidebar.classList.add("collapsed");
+    }
+});
